067fe2a85f3484933adc49b97c03de044f4157ad
Bergrebell <rrkuepper@gmail.com>
2024-02-29 15:39:51 +0100

    add: frame tag helper

--- lib/turbo_clone/engine.rb
+++ lib/turbo_clone/engine.rb
@@ -1,5 +1,13 @@
 module TurboClone
   class Engine < ::Rails::Engine
     isolate_namespace TurboClone
+
+    initializer "turbo.helper" do
+      # Runs when action controller base is included
+      ActiveSupport.on_load :action_controller_base do
+        # include all helpers
+        helper TurboClone::Engine.helpers
+      end
+    end
   end
 end
