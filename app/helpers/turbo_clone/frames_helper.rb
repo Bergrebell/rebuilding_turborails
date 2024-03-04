c6635728284f3368a3ab1d599c94deb269cb477e
Bergrebell <rrkuepper@gmail.com>
2024-03-04 16:22:10 +0100

    add: tests for tag helper

--- app/helpers/turbo_clone/frames_helper.rb
+++ app/helpers/turbo_clone/frames_helper.rb
@@ -1,5 +1,6 @@
 module TurboClone::FramesHelper
   def turbo_frame_tag(resource, &block)
+    # respond_to?(:to_key) is true for ActiveRecord objects (eg. Article.first) and false for eg. strings
     id = resource.respond_to?(:to_key) ? dom_id(resource) : resource
 
     tag.turbo_frame(id: id, &block)
