8a846fc55491186c5d185826efc7c53e7365d494
Bergrebell <rrkuepper@gmail.com>
2024-03-05 14:46:07 +0100

    add: integration tests for stream helpers

--- lib/turbo_clone/engine.rb
+++ lib/turbo_clone/engine.rb
@@ -1,3 +1,5 @@
+require "turbo_clone/test_assertions"
+
 module TurboClone
   class Engine < ::Rails::Engine
     isolate_namespace TurboClone
@@ -11,6 +13,25 @@ module TurboClone
       ActiveSupport.on_load :action_controller_base do
         # include all helpers
         helper TurboClone::Engine.helpers
+      end
+    end
+
+    initializer "turbo.test_assertions" do
+      ActiveSupport.on_load :active_support_test_case do
+        include TurboClone::TestAssertions
+      end
+    end
+
+    initializer "turbo.integration_test_request_encoding" do
+      ActiveSupport.on_load :action_dispatch_integration_test do
+        class ActionDispatch::RequestEncoder
+          class TurboStreamEncoder < IdentityEncoder
+            header = [Mime[:turbo_stream], Mime[:html]].join(",")
+            define_method(:accept_header) { header }
+          end
+
+          @encoders[:turbo_stream] = TurboStreamEncoder.new
+        end
       end
     end
   end
