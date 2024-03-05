b1c4485f0e7c7943ba6ff1a6ba3184977f6aad79
Bergrebell <rrkuepper@gmail.com>
2024-03-05 14:17:53 +0100

    add: Turbo Stream Tag Builder (part 3)

--- app/models/turbo_clone/streams/tag_builder.rb
+++ app/models/turbo_clone/streams/tag_builder.rb
@@ -8,18 +8,31 @@ class TurboClone::Streams::TagBuilder
     action :replace, target, content, **rendering, &block
   end
 
+  def update(target, content = nil, **rendering, &block)
+    action :update, target, content, **rendering, &block
+  end
+
+  def prepend(target, content = nil, **rendering, &block)
+    action :prepend, target, content, **rendering, &block
+  end
+
+  def remove(target)
+    action :remove, target
+  end
+
   private
 
   def action(name, target, content = nil, **rendering, &block)
-    template = render_template(target, content, **rendering, &block)
+    template = render_template(target, content, **rendering, &block) unless name == :remove
 
     turbo_stream_action_tag(name, target: target, template: template)
   end
 
   def turbo_stream_action_tag(action, target:, template:)
-    template = "<template>#{template}</template>"
+    template = action == :remove ? "" : "<template>#{template}</template>"
 
     if target = convert_to_turbo_stream_dom_id(target)
+      # %( is another syntax to create strings - useful if string contains a lot of quotes
       %(<turbo-stream target="#{target}" action="#{action}">#{template}</turbo-stream>).html_safe
     else
       raise ArgumentError, "target must be supplied"
@@ -28,7 +41,7 @@ class TurboClone::Streams::TagBuilder
 
   def render_template(target, content = nil, **rendering, &block)
     if content
-      content
+      content.respond_to?(:to_partial_path) ? @view_context.render(partial: content, formats: :html) : content
     elsif block_given?
       @view_context.capture(&block)
     elsif rendering.any?
